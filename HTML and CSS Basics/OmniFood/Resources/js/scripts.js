4583dc1a3ae7e59654877e878ea9a1d4fd1d1aa3
Pranjal Gupta <pranjalg131@gmail.com>
2020-07-07 16:25:43 +0530

    added nav bar and responsive navbar

--- HTML and CSS Basics/OmniFood/Resources/js/scripts.js
+++ HTML and CSS Basics/OmniFood/Resources/js/scripts.js
@@ -63,33 +63,59 @@ $(document).ready(function () {
       }
     });
 
+  /* Animataions for the website */
+
+  $('.js--wp-1').waypoint(function (direction) {
+    $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function (direction) {
+    $('.js--wp-2').addClass('animate__animated animate__slideInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function (direction) {
+    $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function (direction) {
+    $('.js--wp-4').addClass('animate__animated animate__tada');
+  }, {
+    offset: '50%'
+  });
+
+  /* Mobile Navigation */
+
+  $('.js--nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    nav.slideToggle(200);
+  });
+
+
+  //This works basically like a media query for jQuery, where we can take different actions depending on the screen width
+
+  $(window).resize(function () {
+
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    if ($(window).width() > 767) {
+      nav.css("display", "block");
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      nav.css("display", "none");
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+
+  });
 
 });
 
-/* Animataions for the website */
-
-$('.js--wp-1').waypoint(function (direction) {
-  $('.js--wp-1').addClass('animate__animated animate__fadeIn');
-}, {
-  offset: '50%'
-});
-
-$('.js--wp-2').waypoint(function (direction) {
-  $('.js--wp-2').addClass('animate__animated animate__slideInUp');
-}, {
-  offset: '50%'
-});
-
-$('.js--wp-3').waypoint(function (direction) {
-  $('.js--wp-3').addClass('animate__animated animate__fadeIn');
-}, {
-  offset: '50%'
-});
-
-$('.js--wp-4').waypoint(function (direction) {
-  $('.js--wp-4').addClass('animate__animated animate__tada');
-}, {
-  offset: '50%'
-});
 
 // Always add javascript files in order.
