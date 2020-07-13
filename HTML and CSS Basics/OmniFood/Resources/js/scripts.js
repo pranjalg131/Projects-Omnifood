f432e427800cdf309df7426b26661de0ebf280e9
Pranjal Gupta <pranjalg131@gmail.com>
2020-07-13 16:05:49 +0530

    Omnifood Completed

--- HTML and CSS Basics/OmniFood/Resources/js/scripts.js
+++ HTML and CSS Basics/OmniFood/Resources/js/scripts.js
@@ -94,7 +94,18 @@ $(document).ready(function () {
   $('.js--nav-icon').click(function () {
     var nav = $('.js--main-nav');
     nav.slideToggle(200);
+
+    var icon = $('.js--nav-icon i');
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
   });
+
+
 
 
   //This works basically like a media query for jQuery, where we can take different actions depending on the screen width
